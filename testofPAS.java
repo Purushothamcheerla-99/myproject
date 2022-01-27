3aa975a6a2544663bb81e0638b8167f5267f3971
Purushotham cheerla <98093213+Purushothamcheerla-99@users.noreply.github.com>
2022-01-28 00:52:17 +0530

    Add files via upload
    access specifiers

---
+++ testofPAS.java
@@ -0,0 +1,10 @@
+package test_access_specifiers_2;
+import test_access_specifiers.*;
+public class testofPAS extends protected_access_specifiers {
+	public static void main(String[] args) {
+		testofPAS obj = new testofPAS ();   
+	       obj.display();  
+	}
+
+
+}
---
+++ testofpublic.java
@@ -0,0 +1,14 @@
+package test_access_specifiers_2;
+
+import test_access_specifiers.public_access_specifiers;
+
+public class testofpublic {
+
+	public static void main(String[] args) {
+		
+		public_access_specifiers obj = new public_access_specifiers(); 
+        obj.display();  
+
+	}
+
+}
